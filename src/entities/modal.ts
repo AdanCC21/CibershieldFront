524bce0001f7e46806d063d2b9d9b2cbfa74ff67
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2026-07-27 12:49:10 -0700

    remove piechart, warnings, modal

--- src/entities/modal.ts
+++ src/entities/modal.ts
@@ -1,2 +1,13 @@
 export type HeadColor = 'red' | 'primary';
 export type ModalType = 'finish' | 'error' | 'default';
+
+export interface ModalPrompts {
+    title?: string | null
+    message: string
+
+    color?: HeadColor | null
+    modalType?: ModalType | null
+    results?: { correct: number, incorrect: number }
+
+    btnLabel?: string
+}
